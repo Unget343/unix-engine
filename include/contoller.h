0e47bdcc7798be95e8a3d21e1f1ea635e5acd4b2
Unget343 <zaharkovtonuk36@gmail.com>
2025-11-29 23:26:55 +0000

    the framework was moved to a separate directory

---
+++ include/contoller.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdlib>
+using namespace std;
+
+#if defined(_WIN32)
+#       define UXFRAME_OS_WIN
+#       define BASE_GRAPHIC "dwm"
+#       include <Windows.h>
+#elif defined(__APPLE__)
+#       define UXFRAME_OS_APPLE
+#       define BASE_GRAPHIC "qartz"
+#elif defined(__linux__)
+#       define UXFRAME_OS_BASE
+        const char graphics = getenv("GRAPH_MODE");
+        if (graphics != nullptr && strcmp(graphics, "xcb") == 0)
+        {
+#           define BASE_GRAPHIC "xcb"
+        } else if (graphics != nullptr && strcmp(graphics, "wayland") == 0)
+        {
+#           define BASE_GRAPHIC "wayland"
+        }
+
+#endif // _WIN32
+
+typedef signed int Wid;
+typedef long int  WSize;
+typedef signed char WTitle;
+
+#ifndef BASE_GRAPHIC == "xcb"
+#       include <X11/Xlib.h>
+#elif defined(BASE_GRAPHIC) == "wayland"
+#       include <wayland-client.h>
+#       include <wayland-server.h>
+#       include <wayland-protocols.h>
+#elif defined(BASE_GRAPHIC) == "qartz"
+#       ifndef __OBJC__ // Enable Objective-C
+#               include <CoreGraphics/CoreGraphics.h>
+#       endif // __OBJC__
+#endif // BASE_GRAPHIC
